119930e7912bf6f26069137e80c9c8d619e1ac0b
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2024-02-17 20:57:52 -0600

    Update BING.sh

--- BING.sh
+++ BING.sh
@@ -15,15 +15,44 @@ mkdir INBIN_am
 
 for i in $(ls MAP | grep "M" |cut -d'_' -f1 | sort | uniq)
 do
+
+
+#metabat2
+
 metawrap binning --universal \
 -t 30 \
 -m 100 \
 -o INBIN_am/${i}_inbin \
 -a megahit_am/${i}_megahit/final.contigs.fa \
 --metabat2 \
---metabat \
+MAP/${i}_human_unmap_1.fastq \
+MAP/${i}_human_unmap_2.fastq
+
+
+#maxbin2
+
+metawrap binning --universal \
+-t 30 \
+-m 100 \
+-o INBIN_am/${i}_inbin \
+-a megahit_am/${i}_megahit/final.contigs.fa \
 --maxbin2 \
+MAP/${i}_human_unmap_1.fastq \
+MAP/${i}_human_unmap_2.fastq
+
+
+
+#concoct
+
+metawrap binning --universal \
+-t 30 \
+-m 100 \
+-o INBIN_am/${i}_inbin \
+-a megahit_am/${i}_megahit/final.contigs.fa \
 --concoct \
 MAP/${i}_human_unmap_1.fastq \
 MAP/${i}_human_unmap_2.fastq
+
+
+
 done

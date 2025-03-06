47d5826f3defaf576ec2f1250c8d0543755cf8b2
Alejandro Miguel Cisneros Martínez <cima@ciencias.unam.mx>
2025-03-06 09:11:34 -0600

    Update VIBRANT.sh

--- Viruses/VIBRANT.sh
+++ Viruses/VIBRANT.sh
@@ -5,25 +5,31 @@
 # Used after assembling with PENGUIN
 
 # List of MAGs
-#AA_bin_1_orig.fna
-#AM_X1_bin_2_permissive.fna
-#AM_X3_bin_4_strict.fna
-#AM_X4_bin_5_orig.fna
-#AD_bin_1_orig.fna
-#AD_bin_2_strict.fna
-#AD_bin_3_permissive.fna
-#AD_bin_4_permissive.fna
-#AM_X1_bin_3_permissive.fna
-#AM_X3_bin_5_orig.fna
-#AM_X4_bin_6_strict.fna
-#AM_X1_bin_4_strict.fna
-#AM_X4_bin_1_strict.fna
-#AM_X4_bin_7_strict.fna
-#AM_X3_bin_1_permissive.fna
-#AM_X4_bin_2_strict.fna
-#AM_X3_bin_2_permissive.fna
-#AM_X4_bin_3_strict.fna
-#AM_X1_bin_1_permissive.fna
-#AM_X3_bin_3_strict.fna
-#AM_X4_bin_4_strict.fna
-#AT_bin_1_permissive.fna
+# AA_bin_1_orig.fna
+# AD_bin_1_orig.fna
+# AD_bin_2_strict.fna
+# AD_bin_3_permissive.fna
+# AD_bin_4_permissive.fna
+# AM_X1_bin_1_permissive.fna
+# AM_X1_bin_2_permissive.fna
+# AM_X1_bin_3_permissive.fna
+# AM_X1_bin_4_strict.fna
+# AM_X3_bin_1_permissive.fna
+# AM_X3_bin_2_permissive.fna
+# AM_X3_bin_3_strict.fna
+# AM_X3_bin_4_strict.fna
+# AM_X3_bin_5_orig.fna
+# AM_X4_bin_1_strict.fna
+# AM_X4_bin_2_strict.fna
+# AM_X4_bin_3_strict.fna
+# AM_X4_bin_4_strict.fna
+# AM_X4_bin_5_orig.fna
+# AM_X4_bin_6_strict.fna
+# AM_X4_bin_7_strict.fna
+# AT_bin_1_permissive.fna
+
+# List of PenguiN assemblies
+# penguin_scaffolds_AA.fasta
+# penguin_scaffolds_AD.fasta
+# penguin_scaffolds_AM.fasta
+# penguin_scaffolds_AT.fasta
